b4db3b5a8ebe2b27643a9c971d64fe7ef1a6366b
Ivo Oliveira <soidesigncriacao@gmail.com>
2019-09-02 22:48:02 -0300

    Novo projeto Xamarin Mimica

---
+++ App1_Mimica/App1_Mimica/App1_Mimica/Armazenamento/Armazenamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1_Mimica.Model;
+
+namespace App1_Mimica.Armazenamento
+{
+    public class Armazenamento
+    {
+        public static Jogo Jogo { get; set; }
+        public static short RodadaAtual { get; set; }
+
+        public static string[][] Palavras =
+        {
+            //Fácil Pontuação 1
+            new string[] {"Bocejo", "Interruptor", "Planta", "Milho", "Penalti", "Bola", "Ping-Pong"},
+            
+            //Médio Pontuação 3
+            new string[] {"Carpinteiro", "Imã", "Limão", "Abelha", "Magro", "Sacrifício", "Profundidade"},
+
+            //Dificil Pontuação 5
+            new string[] {"Cineasta", "Lanterna", "Batman vs Superman", "Notebook", "Espinhos", "Capacete", "Parede"},
+        };
+    }
+}
